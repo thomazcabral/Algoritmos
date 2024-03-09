b6055d242083d318167bca01f42b30360f2c5736
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2024-03-09 10:37:04 -0300

    Update q3.cpp

--- Lista5/q3.cpp
+++ Lista5/q3.cpp
@@ -2,103 +2,50 @@
 
 using namespace std;
 
-struct Node {
-    int vertex;
-    Node* next;
-};
+struct DSU {
+    int* parent;
+    int* rank;
+    int* size;
 
-struct Graph {
-    int numVertices;
-    Node** adjLists;
-};
+    DSU(int vertices) { //constructor
+        parent = new int[vertices];
+        rank = new int[vertices]();
+        size = new int[vertices]();
 
-struct StackNode {
-    int data;
-    StackNode* next;
-};
-
-void push(StackNode** root, int data) {
-    StackNode* stackNode = new StackNode;
-    stackNode->data = data;
-    stackNode->next = *root;
-    *root = stackNode; //root is the top of the stack
-}
-
-int pop(StackNode** root) {
-    if (*root == nullptr) { //if the stack is empty
-        return -1;
-    }
-
-    StackNode* temp = *root;
-    *root = (*root)->next; //root is now the next element in the stack
-    int popped = temp->data;
-    delete temp;
-    return popped;
-}
-
-bool DFS(Graph* graph, int startVertex, int endVertex) {
-    bool* visited = new bool[graph->numVertices];
-    for(int i = 0; i < graph->numVertices; i++) { //initially all vertices as unvisited
-        visited[i] = false;
-    }
-
-    StackNode* stack = nullptr;
-    push(&stack, startVertex); //push the start vertex to the stack
-
-    while(stack != nullptr) {
-        int currentVertex = pop(&stack); //pop the top vertex from the stack
-        Node* temp = graph->adjLists[currentVertex];
-
-        if(currentVertex == endVertex) { //if we have reached the end vertex the function stops and returns true
-            delete[] visited;
-            return true;
-        }
-
-        if(!visited[currentVertex]) { //if the vertex is unvisited
-            visited[currentVertex] = true;
-            while(temp) {
-                int adjVertex = temp->vertex; 
-                if(adjVertex == endVertex) { //if the adjacent vertex is the end vertex the function stops and returns true
-                    delete[] visited;
-                    return true;
-                }
-                if(!visited[adjVertex]) {
-                    push(&stack, adjVertex); //push the adjacent vertex to the stack
-                }
-                temp = temp->next;
-            }
+        for (int i = 0; i < vertices; i++) { //initializing the parent and size of each set
+            parent[i] = i;
+            size[i] = 1;
         }
     }
-    delete[] visited;
-    return false; //if it didnt returned true before, its because the end vertex was not reached
-}
 
-Node* createNode(int vertex) {
-    Node* newNode = new Node;
-    newNode->vertex = vertex;
-    newNode->next = nullptr;
-    return newNode;
-}
-
-Graph* createGraph(int vertices) {
-    Graph* graph = new Graph;
-    graph->numVertices = vertices;
-    graph->adjLists = new Node*[vertices];
-    for (int i = 0; i < vertices; i++) { //initialize all vertices as nullptr
-        graph->adjLists[i] = nullptr;
+    ~DSU() { //deallocating memory for the arrays 
+        delete[] parent;
+        delete[] rank;
+        delete[] size;
     }
-    return graph;
-}
 
-void addEdge(Graph* graph, int sourceVertex, int destinationVertex) {
-    Node* newNode = createNode(destinationVertex);
-    newNode->next = graph->adjLists[sourceVertex];
-    graph->adjLists[sourceVertex] = newNode; //add an edge from source to destination
+    int find(int set) { //finding the parent of the set
+        if (set != parent[set])
+            parent[set] = find(parent[set]);
+        return parent[set];
+    }
 
-    newNode = createNode(sourceVertex);
-    newNode->next = graph->adjLists[destinationVertex];
-    graph->adjLists[destinationVertex] = newNode; //add an edge from destination to source
-}
+    void union_sets(int setA, int setB) { //union of two sets
+        setA = find(setA);
+        setB = find(setB);
+        
+        if (setA != setB) {
+            if (rank[setA] < rank[setB])
+                swap(setA, setB);
+
+            parent[setB] = setA;
+
+            size[setA] += size[setB];
+            if (rank[setA] == rank[setB])
+                rank[setA]++;
+        }
+    }
+};
 
 void get_horizontal_wall_separation(int wall, int N, int &square1, int &square2) {
     int row = wall / (N - 1);
@@ -131,7 +78,7 @@ int main() {
     for(int i = 0; i < mazes; i++) {
         int N, wallsRemoved, tests;
         cin >> N >> wallsRemoved >> tests;
-        Graph* graph = createGraph(N * N);
+        DSU dsu(N * N);
         for(int j = 0; j < wallsRemoved; j++) {
             int wall;
             cin >> wall;
@@ -144,13 +91,13 @@ int main() {
             } else {
                 get_vertical_wall_separation(wall - ((N - 1) * (num + 1)), N, square1, square2);
             }
-            addEdge(graph, square1, square2);
+            dsu.union_sets(square1, square2);
         }
         
         for(int j = 0; j < tests; j++) {
             int startVertex, endVertex;
             cin >> startVertex >> endVertex;
-            if(DFS(graph, startVertex, endVertex)) {
+            if(dsu.find(startVertex) == dsu.find(endVertex)) {
                 cout << i << "." << j << " 1" << "\n";
             } else {
                 cout << i << "." << j << " 0" << "\n";
